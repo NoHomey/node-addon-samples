19170774be774fff08a9b41769198254646496fa
NoHomey <ivo.stratev.tues@gmail.com>
2016-10-23 22:41:25 +0300

    finished with first addon sample

--- addons/throw_errno_exception/addon.cc
+++ addons/throw_errno_exception/addon.cc
@@ -1,11 +1,23 @@
 #include "nan.h"
 
-NAN_METHOD(/* method */) {
-    info.GetReturnValue().Set(/* return */);
+#define ArgType(arg, Type, msg) \
+if(!info[arg]->Is##Type()) { \
+    return Nan::ThrowTypeError(msg); \
+}
+
+#define Int32Value(arg) info[arg]->Int32Value()
+
+#define StringValue(arg) *(Nan::Utf8String(info[arg]))
+
+NAN_METHOD(ThrowErrnoException) {
+    ArgType(0, Int32, "First Argument Must be an Integer (valid errno)")
+    ArgType(1, String, "Second Argument Must be a String (syscal)")
+    ArgType(2, String, "Third Argument Must be a String (path)")
+    return Nan::ThrowError(Nan::ErrnoException(Int32Value(0), StringValue(1),  NULL, StringValue(2)));   
 }
 
 void InitAll(v8::Local<v8::Object> exports, v8::Local<v8::Object> module) {
-    Nan::SetMethod(module, "exports", /* method */);
+    Nan::SetMethod(module, "exports", ThrowErrnoException);
 }
 
 NODE_MODULE(ioctl, InitAll)
--- addons/throw_errno_exception/test.js
+++ addons/throw_errno_exception/test.js
@@ -1,1 +1,10 @@
 const addon = require('bindings')('addon.node');
+
+try {
+    addon(9, 'open', '/dev/addon/node');
+} catch(err) {
+    console.log('----------------------------------------');
+    console.log(err.errno, err.code, err.syscall, err.path);
+    console.log('----------------------------------------');
+    throw err;
+}
